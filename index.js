8186a36f42114035d1a8f96e1f9a249b3da3d1cb
James Gauld <james.gauld@dwp.gsi.gov.uk>
2018-02-27 11:04:39 +0000

    chore: config for heroku

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening on port ${server.address().port}!`);
+});
